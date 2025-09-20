0e450eb8bd6120f2947853cb966efc1818b1a3c0
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-09-20 23:53:49 +0530

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -18,8 +18,6 @@ function RegisterPage() {
       setError('Registration failed');
     }
   };
-
-  
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="auth-container">
       <h2>Register</h2>
