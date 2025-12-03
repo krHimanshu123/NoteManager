aff6c45a875b4906cfd4aae86c18bddab361ea8e
Himanshu Kumar <162711386+krHimanshu123@users.noreply.github.com>
2025-12-03 23:52:00 +0530

    Add user reference to note schema

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
+  
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   description: { type: String, required: true }
